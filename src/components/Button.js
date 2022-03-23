bcd21c056cd43790a2c4f352319f64e996cdf0cc
BBrandford11 <brandfordbryce@gmail.com>
2022-03-22 21:26:13 -0600

    removes comments

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,9 +9,6 @@ export default function Button(props) {
       " button--danger": props.danger
     });
    
-   // if(onClick) {
-
-   // }
-
+   
   return <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}  >{props.children}</button>;
 }
