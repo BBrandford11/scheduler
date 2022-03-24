ebc1cb48f4fdc42bcb847d563a099f7c1ebd95b2
BBrandford11 <brandfordbryce@gmail.com>
2022-03-23 23:04:36 -0600

    show index.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
-function Appointment(props) {
-  return <article className="appointment">{!props.time &&`No Appointments`}{props.time && `Appointment at ${props.time}`}</article>;
+export default function Appointment(props) {  
+
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} />
+      : <Empty />}
+    </article>
+  );
 }
-
-export default Appointment;
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Fragment } from 'react'
+
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -176,4 +177,23 @@ storiesOf("Appointment", module)
         onSave={action("onSave")}
         oncancel={action("oncancel")}
       />
-    ));
+    ))
+
+    .add("Appointment Empty", () => (
+      <Fragment>
+        <Appointment id={1} time="4pm" />
+        <Appointment time="5pm" />
+      </Fragment>
+    ))
+    .add("Appointment Booked", () => (
+      <Fragment>
+        <Appointment
+          id={1}
+          time="4pm"
+          interview={{ student: "Lydia Miller-Jones", interviewer }}
+        />
+        <Appointment time="5pm" />
+      </Fragment>
+    ))
+    
+    
